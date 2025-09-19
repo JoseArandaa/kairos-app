69e948a5df8033e868992ef00ede464fa3dcffa4
JoseArandaa <jose.aranda.et28@gmail.com>
2025-09-19 00:36:49 -0300

    feat(navigation): add home stack navigator with screens

---
+++ src/navigation/HomeStack.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/home/HomeScreen";
+import HabitsScreen from "../screens/habits/HabitsScreen";
+import { HomeStackParamList } from "../types";
+
+const Stack = createStackNavigator<HomeStackParamList>();
+
+interface HomeNavigatorProps {
+  id?: string;
+  children?: React.ReactNode;
+}
+
+const HomeNavigator: React.FC<HomeNavigatorProps> = ({ id, children }) => {
+  return (
+    <Stack.Navigator
+      initialRouteName="HomeMain"
+      screenOptions={{ headerShown: false }}
+      // @ts-ignore
+      id={id}
+    >
+      {children}
+    </Stack.Navigator>
+  );
+};
+
+const HomeStack: React.FC = () => {
+  return (
+    <HomeNavigator>
+      <Stack.Screen name="HomeMain" component={HomeScreen} options={{ title: "Inicio" }} />
+      <Stack.Screen
+        name="HabitsScreen"
+        component={HabitsScreen}
+        options={{
+          headerShown: true,
+          title: "My Habits",
+          headerBackTitle: "Back",
+        }}
+      />
+    </HomeNavigator>
+  );
+};
+
+export default HomeStack;
